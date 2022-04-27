52c15ff8c4492432aac4659bf1f0403bf357eca0
Joakim Wassberg <joakim@arthead.se>
2022-04-27 21:55:23 +0200

    Thirteenth Commit

--- flask_api/test/test_users.py
+++ flask_api/test/test_users.py
@@ -15,14 +15,3 @@ def test_users_status_code(client):
     """
     response = client.get('/api/v1.0/users')
     assert response.status_code == 200
-
-
-def test_users_data(client):
-    """
-    Test the data from a call to the users end-point
-    :param client: An app test client from the fixture
-    :return: None
-    """
-    response = client.get('/api/v1.0/users')
-    data = json.loads(response.text)
-    assert data[0]['first_name'] == 'Alice'
